767913b9e40613e2fd7a88a8d47909f21bdc13e9
shehryartariq <tm678123@gmail.com>
2020-10-17 16:41:22 -0400

    did all the formulas

--- area.java
+++ area.java
@@ -10,6 +10,7 @@ class Area {
     int squ = square(side);
 
     System.out.println("The area of square is " + squ);
+    System.out.println();
 
 
     ///user input for the base and height of triangle
@@ -20,6 +21,7 @@ class Area {
     int tri = triangle(base, height);
 
     System.out.println("The area of triangle is " + tri);
+    System.out.println();
 
 
     ///user input for the width and length of rectangle
@@ -30,6 +32,7 @@ class Area {
     int rec = rectangle(width, Length);
 
     System.out.println("The area of rectangle is " + rec);
+    System.out.println();
 
 
     ///user input for the radius of circle
@@ -38,7 +41,41 @@ class Area {
     double cir = circle(radius);
 
     System.out.println("The area of circle is " + cir);
+    System.out.println();
 
+
+    System.out.println("What's the edge of the cube");
+    double edge = area.nextDouble();
+    double cub = cube(edge);
+
+    System.out.println("The area of cube is " + cub);
+    System.out.println();
+
+
+    double rad = sphere(radius);
+
+    System.out.println("The area of sphere is " + rad);
+    System.out.println();
+
+
+    int rec2 = rectangularPrism(Length, width, height);
+
+    System.out.println("The area of reactangular prism is " + rec2);
+    System.out.println();
+
+
+    System.out.println("What's the first side of triangle prism");
+    int firstside = area.nextInt();
+    System.out.println("What's the second side of triangle prism");
+    int secondside = area.nextInt();
+    System.out.println("What's the third side of triangle prism");
+    int thirdside = area.nextInt();
+    System.out.println("What's the prism height");
+    int prismheight = area.nextInt();
+    int tri2 = trianguarPrism(firstside, secondside, thirdside, prismheight, base, height);
+
+    System.out.println("The area of triangular prism is " + tri2);
+    System.out.println();
 
   }
 
@@ -67,5 +104,23 @@ class Area {
       return(area4);
 
     }
+    public static double cube(double edge){
+      double area5 = 6 * Math.pow(edge,2);
+      return(area5);
+    }
+    public static double sphere(double radius){
+      double area6 = 4*Math.PI * Math.pow(radius, 2);
+      return(area6);
+    }
+    public static int rectangularPrism(int length, int width, int height){
+      int area7 = 2 * ((length * width) + (width * height) + (length * height));
+      return(area7);
+    }
+    public static int trianguarPrism(int firstside, int secondside, int thirdside, int prismheight, int base, int height){
+      int area8 = ((base * height) + (firstside + secondside + thirdside) * prismheight);
+      return(area8);
+    }
+
+
 
 }
